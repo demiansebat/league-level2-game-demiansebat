e6ac048c97a4a24408a65e57b864a7cfbd5aab33
league <league@imac-12.attlocal.net>
2022-07-08 16:55:01 -0700

    working on images, will make random car chooser

--- src/MainCharacter.java
+++ src/MainCharacter.java
@@ -2,17 +2,35 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class MainCharacter extends GameObject{
 public boolean doublejump=false;
+public static BufferedImage image;
+public static boolean needImage = true;
+public static boolean gotImage = false;	
 
 
 	public MainCharacter(int x, int y, int width, int height) {
 		super(x, y, width,height);
 		speed=1;
-		
+	if (needImage) {
+	    loadImage ("car1.png");
+		}
+}
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            
+	        }
+	        needImage = false;
+	    }
 	}
-
 	void update() {
 super.update();
 velocity=velocity+gravity;
@@ -33,8 +51,12 @@ else {
 }
 	}
 	void draw(Graphics g) {
+		  if (gotImage) {
+	    		g.drawImage(image, (int)x, (int)y, width, height, null);
+		  }
+		  else {
 		g.setColor(Color.BLACK);
 		g.fillRect((int)x,(int)y,width,height);
 			
-		}
+		}}
 	}
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,9 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Obstacle extends GameObject{
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
 	public Obstacle(int width, int height) {
 		super(SetupGame.WIDTH-10, SetupGame.HEIGHT/2-40, width, height);
 	}
